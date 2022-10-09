00339bb97f009dd6a49741df5bb646dcd6d23ab9
Ryan Tin <ryantin1995@gmail.com>
2022-10-09 12:07:13 -0700

    refactor into routes

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,13 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+pub mod routes;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
+            .route("/health_check", web::get().to(routes::health_check))
+            .route("/subscriptions", web::post().to(routes::subscriptions))
     })
     .listen(listener)?
     .run();
